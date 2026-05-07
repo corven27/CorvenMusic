fe381dd71cdad610da4c1f55e65d94e3c7679192
corven27 <76068101+corven27@users.noreply.github.com>
2026-05-07 13:32:13 -0300

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,10 +1,64 @@
-defaultConfig {
-        applicationId = "com.corven.music"
-        minSdk = 21
-        targetSdk = 35
-        versionCode = 95
-        versionName = "1.0.Corven"
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-        // Esta línea reemplaza a todas las anteriores para evitar errores de paréntesis
-        resValue("string", "env_name", "CorvenMusic")
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.room)
+    alias(libs.plugins.hilt)
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+            freeCompilerArgs.add("-Xcontext-receivers")
+        }
     }
+    jvm("desktop")
+    sourceSets {
+        all {
+            languageSettings {
+                optIn("org.jetbrains.compose.resources.ExperimentalResourceApi")
+            }
+        }
+        val desktopMain by getting
+        desktopMain.dependencies {
+            implementation(compose.components.resources)
+            implementation(compose.desktop.currentOs)
+            implementation(libs.material.icon.desktop)
+            implementation(libs.vlcj)
+            implementation(libs.coil.network.okhttp)
+            runtimeOnly(libs.kotlinx.coroutines.swing)
+        }
+        androidMain.dependencies {
+            implementation(libs.navigation)
+            implementation(libs.media3.session)
+            implementation(libs.media3.ui)
+            implementation(libs.kotlin.coroutines.guava)
+            implementation(libs.kotlin.concurrent.futures)
+            implementation(libs.newpipe.extractor)
+            implementation(libs.nanojson)
+            implementation(libs.androidx.webkit)
+            implementation(libs.room.backup)
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
