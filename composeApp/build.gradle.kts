b3bbd3e7d75e6aee3ec59194c0b49364e201ed53
corven27 <76068101+corven27@users.noreply.github.com>
2026-05-07 13:48:48 -0300

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -28,13 +28,16 @@ kotlin {
             freeCompilerArgs.add("-Xcontext-receivers")
         }
     }
+
     jvm("desktop")
+
     sourceSets {
         all {
             languageSettings {
                 optIn("org.jetbrains.compose.resources.ExperimentalResourceApi")
             }
         }
+
         val desktopMain by getting
         desktopMain.dependencies {
             implementation(compose.components.resources)
@@ -44,6 +47,7 @@ kotlin {
             implementation(libs.coil.network.okhttp)
             runtimeOnly(libs.kotlinx.coroutines.swing)
         }
+
         androidMain.dependencies {
             implementation(libs.navigation)
             implementation(libs.media3.session)
@@ -55,6 +59,7 @@ kotlin {
             implementation(libs.androidx.webkit)
             implementation(libs.room.backup)
         }
+
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -62,3 +67,186 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+
+            implementation(projects.environment)
+            implementation(projects.piped)
+            implementation(projects.invidious)
+
+            implementation(libs.room)
+            implementation(libs.room.runtime)
+            implementation(libs.room.sqlite.bundled)
+
+            implementation(libs.mediaplayer.kmp)
+            implementation(libs.navigation.kmp)
+
+            implementation(libs.coil.compose.core)
+            implementation(libs.coil.compose)
+            implementation(libs.coil.mp)
+
+            implementation(libs.translator)
+            implementation(libs.reorderable)
+        }
+    }
+}
+
+android {
+    fun Project.propertyOrEmpty(name: String): String {
+        val property = findProperty(name) as String?
+        return property ?: ""
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.corven.music"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 115
+        versionName = "1.0.Corven"
+
+        resValue("string", "app_name", "CorvenMusic")
+    }
+
+    splits {
+        abi {
+            reset()
+            isUniversalApk = true
+        }
+    }
+
+    namespace = "it.fast4x.rimusic"
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            manifestPlaceholders["appName"] = "CorvenMusic-Debug"
+        }
+
+        release {
+            vcsInfo.include = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            manifestPlaceholders["appName"] = "CorvenMusic"
+            signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                val outputFileName = "corvenmusic-${variant.baseName}.apk"
+                output.outputFileName = outputFileName
+            }
+    }
+
+    flavorDimensions += "version"
+    productFlavors {
+        create("full") {
+            dimension = "version"
+            manifestPlaceholders["appName"] = "CorvenMusic"
+        }
+        create("accrescent") {
+            dimension = "version"
+            manifestPlaceholders["appName"] = "CorvenMusic-Acc"
+        }
+    }
+
+    sourceSets.all {
+        kotlin.srcDir("src/$name/kotlin")
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    androidResources {
+        generateLocaleConfig = true
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+compose.resources {
+    publicResClass = true
+    generateResClass = always
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
+dependencies {
+    listOf("kspAndroid", "ksp").forEach {
+        add(it, libs.room.compiler)
+    }
+}
+
+dependencies {
+    implementation(projects.composePersist)
+    implementation(libs.compose.activity)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.util)
+    implementation(libs.compose.ripple)
+    implementation(libs.compose.shimmer)
+    implementation(libs.compose.coil)
+    implementation(libs.palette)
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.datasource.okhttp)
+    implementation(libs.appcompat)
+    implementation(libs.appcompat.resources)
+    implementation(libs.support)
+    implementation(libs.media)
+    implementation(libs.material)
+    implementation(libs.material3)
+    implementation(libs.compose.ui.graphics.android)
+    implementation(libs.constraintlayout)
+    implementation(libs.compose.runtime.livedata)
+    implementation(libs.compose.animation)
+    implementation(libs.kotlin.csv)
+    implementation(libs.monetcompat)
+    implementation(libs.androidmaterial)
+    implementation(libs.timber)
+    implementation(libs.crypto)
+    implementation(libs.logging.interceptor)
+    implementation(libs.math3)
+    implementation(libs.toasty)
+    implementation(libs.haze)
+    implementation(libs.androidyoutubeplayer)
+    implementation(libs.glance.widgets)
+    implementation(libs.kizzy.rpc)
+    implementation(libs.gson)
+    implementation (libs.hypnoticcanvas)
+    implementation (libs.hypnoticcanvas.shaders)
+
+    implementation(libs.room)
+    ksp(libs.room.compiler)
+
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+
+    implementation(projects.environment)
+    implementation(projects.kugou)
+    implementation(projects.lrclib)
+    implementation(projects.piped)
+    coreLibraryDesugaring(libs.desugaring.nio)
+}
